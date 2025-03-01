eaca4836aff1075629efcfeb08a9066c7d25619d
Pardum <anonymosek@gmail.com>
2025-03-01 10:02:18 +0530

    first-commit

---
+++ src/test/java/com/PardumYadav/E_Commerce_Site/ECommerceSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PardumYadav.E_Commerce_Site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
